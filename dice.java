c2face58cfa637aa490cf54bd5d3a261edb7dd9b
kpedroc <karl_kpc@hotmail.com>
2023-09-19 15:04:06 +0200

    Random dice

--- dice.java
+++ dice.java
@@ -2,5 +2,10 @@
 class dice {
 public static void main(String[] args) {
 
+    int min = 1;
+    int max = 6;
+    int random_int = (int)Math.floor(Math.random() *(max - min + 1) + min);
+    System.out.println(random_int);
+    
 }
 }
