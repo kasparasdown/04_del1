4f9f40b27e47e52cf90f5e25ab861e19e5a77539
kpedroc <karl_kpc@hotmail.com>
2023-09-19 15:36:03 +0200

    tilføjet players

--- dice.java
+++ dice.java
@@ -2,14 +2,24 @@
 class dice {
 public static void main(String[] args) {
 
+    var scanner = new java.util.Scanner(System.in);
+
     int min = 1;
     int max = 6;
-    int Dice1 = (int)Math.floor(Math.random() *(max - min + 1) + min);
-    int Dice2 = (int)Math.floor(Math.random() *(max - min + 1) + min);
-    int sum = Dice2 + Dice1;
-    System.out.println("Dice 1: "+Dice1);
-    System.out.println("Dice 2: "+Dice2);
-    System.out.println("sum: :" +sum);
-    
+    int player_1 = 0;
+    int player_2 = 0;
+    while (1>0){
+        System.out.println("It's Player 1's turn, press r to roll: ");
+        var n = scanner.nextLine();
+        if (n.equals("r")) {
+            int Dice1 = (int)Math.floor(Math.random() *(max - min + 1) + min);
+            int Dice2 = (int)Math.floor(Math.random() *(max - min + 1) + min);
+
+            player_1 = player_1 + Dice1 + Dice2;
+            System.out.println("Player 1 has Rolled "+ Dice1+" and "+Dice2);
+            System.out.println("Player 1 has "+ player_1+" points");
+
+        }
+    }        
 }
 }
