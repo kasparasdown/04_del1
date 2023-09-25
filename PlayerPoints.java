c2b9957efb3f5d01dbe7860c6043aa9ef0ab6614
Kasparas <marcinskas.kasparas@gmail.com>
2023-09-25 15:19:16 +0200

    Added the second feature.

--- PlayerPoints.java
+++ PlayerPoints.java
@@ -21,24 +21,32 @@ public class PlayerPoints {
                 //Player1 result
                 if(t==1) {
                     player_1 = player_1 + Dice1 + Dice2;
-                    t++;
                     System.out.println("Player 1 has Rolled " + Dice1 + " and " + Dice2 + "\nPlayer 1 has " + player_1 + " points");
                     if (player_1 >= 40) {
                         System.out.println("\nPlayer 1 has won the game");
                         break;
                     }
+                    if (Dice1 == Dice2) {
+                        System.out.println("Hooray! You just got both dice of the same value. You get an extra turn:)");
+                    continue;
+                    }
                     System.out.println("\nNow its Player 2's turn");
+                    t++;
                 }
                 //Player2 result
                 else if(t==2) {
                     player_2 = player_2 + Dice1 + Dice2;
-                    t--;
                     System.out.println("Player 2 has Rolled " + Dice1 + " and " + Dice2 + "\nPlayer 2 has " + player_2 + " points"+System.lineSeparator());
                     if (player_2 >= 40){
                         System.out.println("\nPlayer 2 has won the game");
                         break;
                     }
+                    if (Dice1 == Dice2) {
+                        System.out.println("Hooray! You just got both dice of the same value. You get an extra turn:)");
+                    continue;
+                    }
                     System.out.println("Now its Player 1's turn");
+                    t--;
                 }
                 else{
                     System.out.println("ERROR CODE");
